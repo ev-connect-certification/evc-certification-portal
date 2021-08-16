33d4224dd7cb4d6cb88d461c4b972b834a177d4d
Samson Zhang <wwsamson@yahoo.com>
2021-08-16 08:39:18 -0700

    fix sign in

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,9 +1,18 @@
 import SEO from "../../components/SEO";
 import H1 from "../../components/H1";
-import Auth from "../../components/supabase/Auth";
+import {Auth} from "@supabase/ui";
 import {supabase} from "../../lib/supabaseClient";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function Signin() {
+    const router = useRouter();
+    const {user} = Auth.useUser();
+
+    useEffect(() => {
+        if (user) router.push("/?newsignin=true");
+    }, [user]);
+
     return (
         <div className="max-w-sm mx-auto my-4 p-6 bg-white rounded border shadow-sm mt-20">
             <SEO/>
