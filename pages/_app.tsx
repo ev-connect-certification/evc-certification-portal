ccb27bf7aa0eb6d08f90adfdadb1201802f29b06
Samson Zhang <wwsamson@yahoo.com>
2021-08-16 13:45:57 -0700

    add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,14 @@ import Navbar from "../components/Navbar";
 import {Auth} from "@supabase/ui";
 import {supabase} from "../lib/supabaseClient";
 import {ToastProvider} from "react-toast-notifications";
+import {Router} from "next/router";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 
 function MyApp({Component, pageProps}) {
     return (
